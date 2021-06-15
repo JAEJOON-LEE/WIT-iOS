3d8b95aacc7296db511a7b5448a9647122f47430
LEESEUNGMIN <40168455+leeseungmin4966@users.noreply.github.com>
2021-06-16 04:53:24 +0900

    Post Image

--- WIT_combine/TimeLine/TimeLineView/Screen/Root/TimeLineView.swift
+++ WIT_combine/TimeLine/TimeLineView/Screen/Root/TimeLineView.swift
@@ -10,9 +10,12 @@ import SwiftUI
 struct TimeLineView: View {
     
     var colWidth : CGFloat
-    
+    @State var tag:Int? = nil
     var body: some View {
         ZStack(alignment : .bottomTrailing) {
+            NavigationLink(destination: PostView(), tag: 1, selection: self.$tag ) {
+                 EmptyView()
+               }
             VStack{
                 //navigation field
                 ScrollView{
@@ -21,7 +24,6 @@ struct TimeLineView: View {
                             .padding(.leading,10)
                             .padding(.trailing,10)
                             .padding(.bottom,15)
-        
                         PersonBodyView(colWidth:colWidth)
                             .padding(.leading,5)
                             .padding(.trailing,5)
@@ -31,14 +33,16 @@ struct TimeLineView: View {
                 }
             }
             Button {
+                self.tag = 1
             } label : {
-                Image(systemName: "rectangle.3.offgrid.fill")
-                    .font(.system(size : 30))
+                Image(systemName: "camera.circle.fill")
+                    .frame(width: 25, height: 25)
+                    .font(.system(size : 45))
                     .foregroundColor(.white)
                     .padding()
                     .background(Color.blue)
                     .clipShape(Circle())
-                    .padding(15)
+                    .padding(8)
                     .padding(.bottom,40)
             }
         }
