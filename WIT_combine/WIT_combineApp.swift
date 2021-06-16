de3b5ebeaa6993ae83561da14e680ed2538a1850
LEESEUNGMIN <40168455+leeseungmin4966@users.noreply.github.com>
2021-06-16 20:52:06 +0900

    db connected

--- WIT_combine/WIT_combineApp.swift
+++ WIT_combine/WIT_combineApp.swift
@@ -1,7 +1,11 @@
 import SwiftUI
-
+import Firebase
 @main
 struct WITApp: App {
+    
+    //for firebasce connection
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
@@ -20,3 +24,12 @@ class Store: ObservableObject {
     }
 }
 
+//firebase connection
+class AppDelegate: NSObject, UIApplicationDelegate{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        print("Firevase is connected")
+        FirebaseApp.configure()
+        return true
+    }
+}
